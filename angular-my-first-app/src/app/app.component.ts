46ed0fd6ce831b8c02972af9d83c450c048f49a4
DouglasCandido <douglasmateus1@hotmail.com>
2021-02-12 09:47:18 -0300

    Área de conteúdo adicionada.

--- angular-my-first-app/src/app/app.component.ts
+++ angular-my-first-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Controle do Almoxarifado';
+export class AppComponent implements AfterViewInit {
+  title = 'Controle do Almoxarifado da UFRN';
+  constructor(private elementRef: ElementRef) {
+
+  }
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = "gainsboro";
+  }
 }
