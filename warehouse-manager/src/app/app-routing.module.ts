d384cc9c5fd21e227fc18acbda34a9e14e298c3e
DouglasCandido <douglasmateus1@hotmail.com>
2021-02-13 23:21:02 -0300

    Minor changes.

--- warehouse-manager/src/app/app-routing.module.ts
+++ warehouse-manager/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { MapComponent } from './views/map/map.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'products', component: ProductCrudComponent }
+  { path: 'products', component: ProductCrudComponent },
+  { path: 'location', component: MapComponent}
 ];
 
 @NgModule({
